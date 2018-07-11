bbf8207739fc05204f4a57b4c70e6f28faf8262e
Trizzlenova <tdswayzee@gmail.com>
2018-07-11 09:18:30 -0700

    had to start new repo for app

--- .env/bin/activate.fish
+++ .env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/TDSwayzee22/Desktop/wdi/chord-leap/backend/.env"
+set -gx VIRTUAL_ENV "/Users/TDSwayzee22/Desktop/wdi/chord-leap-backend/backend/.env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- .env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/TDSwayzee22/Desktop/wdi/chord-leap/backend/.env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
