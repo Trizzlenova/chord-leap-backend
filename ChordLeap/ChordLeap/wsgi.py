3467428377334fe8fd81b43a8ada7c7254a9a23b
Trizzlenova <tdswayzee@gmail.com>
2018-07-12 11:37:03 -0700

    hjsda

--- ChordLeap/ChordLeap/wsgi.py
+++ ChordLeap/ChordLeap/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChordLeap.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChordLeap.ChordLeap.settings")
 
 application = get_wsgi_application()
 
